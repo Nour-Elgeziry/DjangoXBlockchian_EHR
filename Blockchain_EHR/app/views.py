646712c75108dff7e035506d08636058d919dd00
Nour <nourahmedelgeziry@gmail.com>
2021-07-22 13:47:10 +0100

    added Home page to display information + added functions to register users, get and add information

--- Blockchain_EHR/app/views.py
+++ Blockchain_EHR/app/views.py
@@ -1,5 +1,5 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 ''' Create your views here. '''
 from django.http import HttpResponse
@@ -17,29 +17,64 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 print('user account', web3.eth.defaultAccount)
 
 '''setting the contract'''
-contract_address = web3.toChecksumAddress("0xF9d78006A806c35cef9F32A508e97A4F271C826b") 
-contract_abi = json.loads('[{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"Patients","outputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"dob","type":"string"},{"internalType":"string","name":"postCode","type":"string"},{"internalType":"string","name":"bloodType","type":"string"},{"internalType":"string","name":"weight","type":"string"},{"internalType":"string","name":"height","type":"string"},{"internalType":"string","name":"insurance","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getAllergies","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getInsurance","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMedication","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPatientInfo","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_allergies","type":"string"}],"name":"setAllergies","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_insurance","type":"string"}],"name":"setInsurance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_medication","type":"string"}],"name":"setMedication","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_dob","type":"string"},{"internalType":"string","name":"_postCode","type":"string"},{"internalType":"string","name":"_weight","type":"string"},{"internalType":"string","name":"_height","type":"string"}],"name":"setingPatientInfo","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]')
-                            
+contract_address = web3.toChecksumAddress("0x1dD4989338241041beE8922F9aaF2714461E3747") 
+contract_abi = json.loads('[{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"Patients","outputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"dob","type":"string"},{"internalType":"string","name":"postCode","type":"string"},{"internalType":"string","name":"bloodType","type":"string"},{"internalType":"string","name":"weight","type":"string"},{"internalType":"string","name":"height","type":"string"},{"internalType":"string","name":"insurance","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"deleteMedication","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getAllergies","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getInsurance","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMedication","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPatientInfo","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_allergies","type":"string"}],"name":"setAllergies","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_insurance","type":"string"}],"name":"setInsurance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_medication","type":"string"}],"name":"setMedication","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_dob","type":"string"},{"internalType":"string","name":"_postCode","type":"string"},{"internalType":"string","name":"_weight","type":"string"},{"internalType":"string","name":"_height","type":"string"}],"name":"setingPatientInfo","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]')
+                        
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
-'''call register patient function'''
-tx_hash = contract.functions.setingPatientInfo('Nour', '07.07.1999', 'CV14GJ', '80', '183').transact()
-'''wait for transaction reciept'''
-web3.eth.waitForTransactionReceipt(tx_hash)
-''' get the username'''
-print('returned user info', contract.functions.getPatientInfo().call())
 
-''' set  patient medication'''
-tx_hash = contract.functions.setMedication('panadol').transact()
-'''wait for transaction reciept'''
-web3.eth.waitForTransactionReceipt(tx_hash)
-''' get the username'''
-print('returned user medication', contract.functions.getMedication().call())
+def home(request):
+    print('Inside home')
+    user_info = contract.functions.getPatientInfo().call()
+    name = user_info[0]
+    dob =  user_info[1]
+    postCode = user_info[2]
+    height = user_info[3]
+    weight = user_info[4]
 
+    user_insurance = contract.functions.getInsurance().call()
+    user_medication = contract.functions.getMedication().call()
+    user_allergies = contract.functions.getAllergies().call()
 
+    print('user medication', user_medication)
 
-'''functions that carry different transactions'''
-def index(request):
-    return HttpResponse("Hello, world. You're at the Blockchain EHR website.")
+    context ={
+        'name': name,
+        'dob': dob,
+        'postCode': postCode,
+        'height' : height,
+        'weight' : weight,
+        'insurance': user_insurance,
+        'medication': user_medication,
+        'allergies': user_allergies
+    }
+    return render(request, 'app/home.html', context)
 
+def registerPatient(request):
+    print('registering patient')
+    #tx_hash = contract.functions.setingPatientInfo('Nour', '07.07.1999', 'CV14GJ', '80', '183').transact()
+    '''wait for transaction reciept'''
+    #web3.eth.waitForTransactionReceipt(tx_hash)
+    return redirect('/')
+
+def registerInsurance(request):
+    print('registering insurance')
+    #tx_hash = contract.functions.setInsurance(str(insurance_no)).transact()
+    '''wait for transaction reciept'''
+    #web3.eth.waitForTransactionReceipt(tx_hash)
+    return redirect('/')
+
+def registerMedication(request):
+    print('registering medication')
+    #tx_hash = contract.functions.setMedication('medication').transact()
+    '''wait for transaction reciept'''
+    #web3.eth.waitForTransactionReceipt(tx_hash)
+    return redirect('/')
+
+def registerAllergies(request):
+    print('registering allergies')
+    #tx_hash = contract.functions.setAllergies('allergy').transact()
+    '''wait for transaction reciept'''
+    #web3.eth.waitForTransactionReceipt(tx_hash)
+    return redirect('/')
 
